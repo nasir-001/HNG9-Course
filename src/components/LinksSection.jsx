beac49bba695cc87a34e9df8b840e0ca5184538f
Nasir Lawal <nasirlawal001@gmail.com>
2022-11-03 22:35:53 +0100

    added contact page

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -1,66 +1,68 @@
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import React from 'react';
 import slack from '../assets/svg/slack.svg';
 import github from '../assets/svg/github.svg'
 
 const LinksSection = () => {
   return (
-    <div className='lg:mx-8'>
-      <div className='mx-4 md:mx-12 lg:w-2/3 lg:mx-auto mt-12'>
-        <a className='font-semibold' id='twitter' href="http://twitter.com/_code_for_fun" target="_blank" rel="noopener noreferrer">
-          <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+    <div className='lg:tw-mx-8'>
+      <div className='tw-mx-4 md:tw-mx-12 lg:tw-w-2/3 lg:tw-mx-auto tw-mt-12'>
+        <a className='tw-font-semibold' id='twitter' href="http://twitter.com/_code_for_fun" target="_blank" rel="noopener noreferrer">
+          <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
             Twitter Link
           </div>
         </a>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='btn__zuri' href="https://training.zuri.team/" target="_blank" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+        <div className='tw-mt-4'>
+          <a type='submit' className='tw-font-semibold' id='btn__zuri' href="https://training.zuri.team/" target="_blank" rel="noopener noreferrer">
+            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
               Zuri Team
             </div>
           </a>
         </div>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='books' href="http://books.zuri.team" target="_blank" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+        <div className='tw-mt-4'>
+          <a type='submit' className='tw-font-semibold' id='books' href="http://books.zuri.team" target="_blank" rel="noopener noreferrer">
+            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
               Zuri Books
             </div>
           </a>
         </div>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=Nasir" target="_blank" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+        <div className='tw-mt-4'>
+          <a type='submit' className='tw-font-semibold' id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=Nasir" target="_blank" rel="noopener noreferrer">
+            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
               Python Books
             </div>
           </a>
         </div>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='pitch' href="https://background.zuri.team" target="_blank" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+        <div className='tw-mt-4'>
+          <a type='submit' className='tw-font-semibold' id='pitch' href="https://background.zuri.team" target="_blank" rel="noopener noreferrer">
+            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
               Background Check for Coders
             </div>
           </a>
         </div>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='book__design' href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
+        <div className='tw-mt-4'>
+          <a type='submit' className='tw-font-semibold' id='book__design' href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer">
+            <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
               Design Books
             </div>
           </a>
         </div>
-        <div className='mt-4'>
-          <a type='submit' className='font-semibold' id='book__design' href="/contact" rel="noopener noreferrer">
-            <div className='bg-gray-200 text-center w-full py-3 h-12 rounded-lg'>
-              Contact
-            </div>
-          </a>
+        <div className='tw-mt-4'>
+          <Link to='/contact' id='book__design'>
+            <a className='tw-font-semibold'>
+              <div className='tw-bg-gray-200 tw-text-black tw-text-center tw-w-full tw-py-3 tw-h-12 tw-rounded-lg'>
+                Contact
+              </div>
+            </a>
+          </Link>
         </div>
-        <div className='flex justify-center mt-8'>
-          <div className='mx-2'>
+        <div className='tw-flex tw-justify-center tw-mt-8'>
+          <div className='tw-mx-2'>
             <a href="https://hng9.slack.com/archives/D0487REQHD5" target="_blank" rel="noopener noreferrer">
               <img src={slack} alt="slack" />
             </a>
           </div>
-          <div className='mx-2'>
+          <div className='tw-mx-2'>
             <a href="https://github.com/nasir-001" target="_blank" rel="noopener noreferrer">
               <img src={github} alt="github" />
             </a>
--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -6,20 +6,20 @@ import React from 'react'
 
 const ProfileSection = () => {
   return (
-    <div className='mt-14'>
-      <div className='flex'>
-        <div className='w-1/2'></div>
-        <div className='flex -ml-12'>
+    <div className='tw-mt-14'>
+      <div className='tw-flex'>
+        <div className='tw-w-1/2'></div>
+        <div className='tw-flex -tw-ml-12'>
           <div>
-            <img src={image} className='mx-auto w-20 h-2w-20 rounded-full' />
+            <img src={image} className='tw-mx-auto tw-w-20 tw-h-2w-20 tw-rounded-full' />
           </div>
-          <div className='w-10 h-10 md:w-12 md:h-12 ml-14 lg:ml-56 rounded-full border border-gray-200 border-dashed cursor-pointer'>
-            <img id='profile__img' src={share} alt="share" className='m-3.5 hidden md:block' />
-            <img id='profile__img' src={elipsis} alt="share" className='mx-3 mt-4 md:hidden' />
+          <div className='tw-w-10 tw-h-10 md:tw-w-12 md md:tw-h-12 tw-ml-14 lg:tw-ml-56 tw-rounded-full tw-border tw-border-gray-200 tw-border-dashed tw-cursor-pointer'>
+            <img id='profile__img' src={share} alt="share" className='tw-m-3.5 tw-hidden md:tw-block' />
+            <img id='profile__img' src={elipsis} alt="share" className='tw-mx-3 tw-mt-4 md:tw-hidden' />
           </div>
         </div>
       </div>
-      <div className='pt-2 text-center text-2xl font-semibold -ml-2'>
+      <div className='tw-pt-2 tw-text-center tw-text-2xl tw-font-semibold -tw-ml-2'>
         Nasir Lawal
       </div>
     </div>
